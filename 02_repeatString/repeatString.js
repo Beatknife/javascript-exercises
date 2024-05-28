82caf9cefec0b1b28e0783a77ecb9818a18df274
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-05-28 11:03:58 +0300

    JS exercise 3 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -3,7 +3,7 @@ const repeatString = function(string, num) {
     if (num > 0) {
         return string.repeat(num);
     } else if (num < 0) {
-        return "ERROR";
+        return "ERROR";gi
     } else {
         return "";
     }
