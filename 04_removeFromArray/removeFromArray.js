daf55e2074ff5331d0d192b1bc9d9e96f424db24
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-05-29 23:13:50 +0300

    JS exercise 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,8 @@
 const removeFromArray = function(array, ...multipleArgs) {
     
-    multipleArgs.forEach((eachArg) => {
-        
-        for (let i = 0; i < array.length; i++) {
-            if( array[i] === eachArg){
-                array.splice(i, 1);
-            } 
-            
-        }  
-    });
+    return array.filter((item) => !multipleArgs.includes(item))
+    };
 
-    return array;
-}
 
 // Do not edit below this line
 module.exports = removeFromArray;
