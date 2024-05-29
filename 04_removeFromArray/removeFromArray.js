d31251db744e33a7783c5938dc6c134e171bc1dd
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-05-29 18:33:44 +0300

    Pass every test except removing same value in multiple places

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...multipleArgs) {
+    
+    multipleArgs.forEach((eachArg) => {
+        
+        for (let i = 0; i < array.length; i++) {
+            if( array[i] === eachArg){
+                array.splice(i, 1);
+            } 
+            
+        }  
+    });
+
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
