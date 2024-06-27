30a8531ad2c540c815464785e43d00faa901a8e8
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-06-27 11:47:23 +0300

    JS exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,9 +1,23 @@
 const fibonacci = function(n) {
 
-    if (n <= 2 && n >= 0 ) return n
-    if (n < 0) return "OOPS"
+    let nthNumber = parseInt(n);
 
-    
+    if (nthNumber === 1 || nthNumber === 0) return nthNumber;
+    if (nthNumber === 2) return 1;
+    if (nthNumber < 0) return "OOPS"
+
+    let prevNum = 0;
+    let nextNum = 1;
+
+    for (let i = 2; i <= nthNumber; i++) {
+
+        let fibValue = prevNum + nextNum;
+        prevNum = nextNum;
+        nextNum = fibValue;
+
+    }
+
+    return nextNum;
 
 };
 
