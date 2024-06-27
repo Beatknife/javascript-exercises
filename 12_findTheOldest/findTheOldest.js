17407c2c6ddf886423a3994b47f21af04960f067
Yahya Bulat <138808138+Beatknife@users.noreply.github.com>
2024-06-27 13:43:35 +0300

    JS exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
 
+function calculateAge(person) {
+    const deathYear = person.yearOfDeath || currentYear;
+    return deathYear - person.yearOfBirth 
+}
+
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldestAge = calculateAge(oldest);
+        const currentAge = calculateAge(current);
+        return currentAge > oldestAge ? current : oldest;
+    })
 };
 
 // Do not edit below this line
